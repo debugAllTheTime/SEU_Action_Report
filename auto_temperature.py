7aa0cc7e05c2ca22814ced30226f47613ca20a36
naokuoyoudiantong <774040105@qq.com>
2021-12-02 13:47:00 +0800

    Update auto_temperature.py

--- auto_temperature.py
+++ auto_temperature.py
@@ -83,7 +83,7 @@ def main():
         print("点击保存")
         time.sleep(t)
 
-        driver.find_element_by_xpath('/html/body/div[60]/div[1]/div[1]/div[2]/div[2]/a[1]').click()
+        driver.find_element_by_xpath('/html/body/div[63]/div[1]/div[1]/div[2]/div[2]/a[1]').click()
         print(name + '\t体温上报成功')
         msg += name + '\t体温上报成功' + '\n\n'
         driver.quit()
